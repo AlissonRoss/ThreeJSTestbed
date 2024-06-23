d82391deb4328712bff60d7ce69a193585ce2af3
AlissonRoss <alisson@alisson.me>
2024-06-23 04:14:25 -0700

    Fixed camera position, added axes helpers, changes intensity of lights

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import { Canvas} from "@react-three/fiber";
 import {Model} from "./Tower.js";
-
-import { OrbitControls } from '@react-three/drei';
+import { Text } from "@react-three/drei"
+import { OrbitControls} from '@react-three/drei';
 import "./styles.css";
 
 export default function App() {
   return (
-    <Canvas>
+    <Canvas camera={{ position: [30, 35, 60] }}>
       <OrbitControls />
-      <ambientLight intensity={0.1} />
-      <directionalLight intensity={0.5} />
+      <ambientLight intensity={0.5} />
+      <directionalLight intensity={50} />
       <Suspense fallback={null}>
-        <Model/>
+        <Model position={[0, 0, 0]}/>
+        <Text
+          position={[0, 0, 0]} // Position the text in the scene
+          fontSize={5} // Adjust the font size
+          color="white" // Text color
+        >
+          Hackmerced X Testbed
+        </Text>
       </Suspense>
     </Canvas>
   );
